cd0a9d84a04a50cd0d4ee16ae0c8b25dcacb0761
Solo4 <akasha02@mail.ru>
2021-04-03 15:12:57 +0300

    DrawAnomalyCircle.kt создаёт круг на карте

---
+++ app/src/main/java/com/android/radworld/ui/DrawAnomalyCircle.kt
@@ -0,0 +1,23 @@
+package com.android.radworld.ui
+
+import android.graphics.Color
+import com.google.android.gms.maps.model.CircleOptions
+import com.google.android.gms.maps.model.LatLng
+
+/**
+ * отрисовывает круг по принятив координатом в формате LatLng
+ *
+ * возвращает CircleOptions
+ *
+ * далее -> googleMap.addCircle(полученный результат из функции)
+ */
+
+class DrawAnomalyCircle {
+    fun drawAnomaly(latLon: LatLng) = CircleOptions().apply {
+        center(latLon)
+        radius(20.0)
+        strokeColor(Color.RED)
+        fillColor(Color.YELLOW)
+        strokeWidth(2F)
+    }
+}
